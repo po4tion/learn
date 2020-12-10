91ad80cf56945edcb6b77f6afb13d160213c3e62
DongKyu <po4tion0429@gmail.com>
2020-12-11 01:13:20 +0900

    list

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,42 +1,97 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
-      { name: 'Dong-Gyu', age: 26 },
-      { name: 'Myung-Gyu', age: 24 },
-      { name: 'Max', age: 28 }
+      { id: 'a', name: 'Dong-Gyu', age: 26 },
+      { id: 'b', name: 'Myung-Gyu', age: 24 },
+      { id: 'c', name: 'Max', age: 28 }
     ]
-  });
+  }
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
+  switchNameHandler = newName => {
     // console.log('Was clicked!');
     // DONT'T DO THIS => this.state.persons[0].name = 'Kim Dong Gyu';
-    setPersonsState({
+    this.setState({
       persons: [
-        { name: 'Kim Dong-Gyu', age: 26 },
+        { name: newName, age: 26 },
         { name: 'Kim Myung-Gyu', age: 24 },
         { name: 'Maximilian', age: 28 }
-      ]
+      ],
+      otherState: 'some other state',
+      showPersons: false
     });
   };
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies : Racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
-    </div>
-  );
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons });
+  }
+
+  deletePersonHandler = personIndex => {
+    // const persons = this.state.persons;
+    const persons = [...this.state.persons];
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons });
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name</button>
+        {persons}
+      </div>
+    );
+  }
   // return React.createElement('div', { className: 'App' },
   //   React.createElement('h1', null, "I\'m a React App!!!"));
   // html에서 onclick, jsx에서는 onClick
