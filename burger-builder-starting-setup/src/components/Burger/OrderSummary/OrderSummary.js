6fb9ca4aa13f3d936a3ca5fd89a99e08c78c7cc4
DongKyu <po4tion0429@gmail.com>
2020-12-24 05:54:26 +0900

    add lifecycle

--- burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder-starting-setup/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxi';
+import React, { Component } from 'react';
+import Aux from '../../../hoc/Auxi/Auxi';
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component { 
+  componentWillUpdate () {
+    console.log('[OrderSummary] WillUpdate');
+  }
+
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
     .map(igKey => {
-      return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}</li>
+      return <li key={igKey}><span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}</li>
     });
 
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total Price: {props.price}</strong></p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCanclled}>CANCEL</Button>
-      <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total Price: {this.props.price}</strong></p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Danger" clicked={this.props.purchaseCanclled}>CANCEL</Button>
+        <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
+      </Aux>
+    );
+  }
+}
 
 export default OrderSummary;
