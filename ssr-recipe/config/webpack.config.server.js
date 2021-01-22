3692145895e481accc31b6c930cb75904cfc0e5a
DongKyu <po4tion0429@gmail.com>
2021-01-22 13:01:27 +0900

    eject & set webpack

---
+++ ssr-recipe/config/webpack.config.server.js
@@ -0,0 +1,16 @@
+const paths = require('./paths');
+
+module.exports = {
+  mode: 'production',
+  entry: paths.ssrIndexJs,
+  target: 'node',
+  output: {
+    path: paths.ssrBuild,
+    filename: 'server.js',
+    chunkFilename: 'js/[name].chunk.js',
+    publicPath: paths.publicUrlOrPath,
+  },
+  resolve: {
+    modules: ['node_modules'],
+  },
+};
