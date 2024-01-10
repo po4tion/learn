2b4326d66ad8671b48e39ca87c7054c6f3105044
po4tion <po4tion0429@gmail.com>
2024-01-11 01:22:45 +0900

    feat(7): NodeList and HTMLCollection

--- javascript/JavaScript-basics/basics-07/dom/app.js
+++ javascript/JavaScript-basics/basics-07/dom/app.js
@@ -1,6 +1,18 @@
-window.alert("Hello");
-alert("Hello"); // 다른 곳에서 찾을 수 없는 함수라면 자동으로 앞에 window가 추가된다.
+// window.alert("Hello");
+// alert("Hello"); // 다른 곳에서 찾을 수 없는 함수라면 자동으로 앞에 window가 추가된다.
 
 /* 
   window는 document를 포함하고 있다.
 */
+
+// DOM control
+const title = document.getElementById("title");
+console.log(title);
+
+console.log("-------class-------");
+const listItemList = document.getElementsByClassName("listItem");
+console.log(listItemList);
+
+console.log("-------query-------");
+const listItemListByQuery = document.querySelectorAll(".listItem");
+console.log(listItemListByQuery);
