33f5c05d85eaec9e4f8ff619caf1bdea797b561c
po4tion <po4tion0429@gmail.com>
2024-01-14 02:14:54 +0900

    feat(2): 수동 메모리 관리 정리

--- rust/building-real-application/example/src/main.rs
+++ rust/building-real-application/example/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let a = 2;
+    let result = stack_only(a);
+
+    dbg!(result);
+}
+
+// i32 : 정수형 32비트
+fn stack_only(b: i32) -> i32 {
+    let c = 3;
+
+    return b + c + stack_and_heap();
+}
+
+fn stack_and_heap() -> i32 {
+    let d = 5;
+    // Box는 rust의 스마트 포인터 타입
+    let e = Box::new(7);
+
+    return d + *e;
 }
