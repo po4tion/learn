332e8eb4b7977461cc7bfff2b8086e71f0798078
po4tion <po4tion0429@gmail.com>
2024-01-14 00:02:55 +0900

    feat(7): 화면 전체가 리렌더링 되지 않도록 이벤트 적용해보기

--- javascript/JavaScript-basics/basics-07/dom-styling/app.js
+++ javascript/JavaScript-basics/basics-07/dom-styling/app.js
@@ -5,10 +5,14 @@ console.log(firstLi);
 
 const section = document.querySelector("section");
 const button = document.querySelector("button");
+const div = document.querySelector("div");
 
 // section.style.backgroundColor = 'blue';
 section.className = "red-bg";
 
+/* 
+  스타일 적용 우선순위는 인라인이 가장 높다
+*/
 button.addEventListener("click", () => {
   // if (section.className === 'red-bg visible') {
   //   section.className = 'red-bg invisible';
@@ -18,8 +22,14 @@ button.addEventListener("click", () => {
 
   // section.classList.toggle('visible');
   section.classList.toggle("invisible");
+
+  // innerHTMl로 바뀐 컨텐츠는 다시 렌더링된다. 그래서 자식 엘리먼트인 Input이 계속 초기화된다
+  div.innerHTML += "<p>Rerender</p>";
 });
 
-/* 
-  스타일 적용 우선순위는 인라인이 가장 높다
-*/
+const noRerenderButton = document.getElementById("no-rerender");
+
+noRerenderButton.addEventListener("click", () => {
+  // insertAdjacentHTML는 전체를 리렌더링 하지 않고 새로운 코드만 렌더링한다
+  div.insertAdjacentHTML("beforeend", "<p>No Rerender</p>");
+});
