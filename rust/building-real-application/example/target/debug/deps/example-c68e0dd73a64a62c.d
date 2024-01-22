96501292e05e3ee81338acfcb8777731db62b8d5
po4tion <po4tion0429@gmail.com>
2024-01-22 21:24:35 +0900

    chore: update debug

--- rust/building-real-application/example/target/debug/deps/example-c68e0dd73a64a62c.d
+++ rust/building-real-application/example/target/debug/deps/example-c68e0dd73a64a62c.d
@@ -1,5 +1,5 @@
-/Users/po4tion/Desktop/study/learn/rust/building-real-application/example/target/debug/deps/libexample-c68e0dd73a64a62c.rmeta: src/main.rs
+/Users/po4tion/Desktop/projects/learn/rust/building-real-application/example/target/debug/deps/libexample-c68e0dd73a64a62c.rmeta: src/main.rs
 
-/Users/po4tion/Desktop/study/learn/rust/building-real-application/example/target/debug/deps/example-c68e0dd73a64a62c.d: src/main.rs
+/Users/po4tion/Desktop/projects/learn/rust/building-real-application/example/target/debug/deps/example-c68e0dd73a64a62c.d: src/main.rs
 
 src/main.rs:
--- rust/building-real-application/gdb/target/debug/gdb.d
+++ rust/building-real-application/gdb/target/debug/gdb.d
@@ -1,1 +1,1 @@
-/Users/po4tion/Desktop/study/learn/rust/building-real-application/gdb/target/debug/gdb: /Users/po4tion/Desktop/study/learn/rust/building-real-application/gdb/src/main.rs
+/Users/po4tion/Desktop/projects/learn/rust/building-real-application/gdb/target/debug/gdb: /Users/po4tion/Desktop/projects/learn/rust/building-real-application/gdb/src/main.rs
