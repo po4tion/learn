6179bd1f6ff5b014f6484e0161be0c589d3a9041
po4tion <po4tion0429@gmail.com>
2024-05-04 00:25:22 +0900

    feat: DTO 추가

--- nestjs/messages/src/messages/messages.controller.ts
+++ nestjs/messages/src/messages/messages.controller.ts
@@ -1,4 +1,5 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDTO } from './dtos/create-message.dto';
 
 // class decorator
 @Controller('messages')
@@ -8,7 +9,7 @@ export class MessagesController {
   listMessages() {}
 
   @Post()
-  createMessage(@Body() body: any) {
+  createMessage(@Body() body: CreateMessageDTO) {
     console.log(body);
   }
 
