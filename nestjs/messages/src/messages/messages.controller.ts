7bb3426673213746e4485cbaacacc9ee2b048941
po4tion <po4tion0429@gmail.com>
2024-05-04 02:37:48 +0900

    feat: GET, POST 테스트

--- nestjs/messages/src/messages/messages.controller.ts
+++ nestjs/messages/src/messages/messages.controller.ts
@@ -1,21 +1,30 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { CreateMessageDTO } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 // class decorator
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   // method decorator
   @Get()
-  listMessages() {}
+  listMessages() {
+    return this.messagesService.findAll();
+  }
 
   @Post()
   createMessage(@Body() body: CreateMessageDTO) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   // argument decorator
   getMessage(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
