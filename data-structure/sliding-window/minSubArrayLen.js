f1ab047f55b2f4f7d9abc3a4d58412efe674a397
po4tion <po4tion0429@gmail.com>
2024-05-16 22:24:43 +0900

    solve(recursion): power

--- data-structure/sliding-window/minSubArrayLen.js
+++ data-structure/sliding-window/minSubArrayLen.js
@@ -24,7 +24,7 @@ function minSubArrayLen(inputs, number) {
 /* 
   Time: O(n)
   Space: O(1)  
-2 5 6 6
+
   minSubArrayLen([2,3,1,2,4,3], 7) // 2 -> because [4,3] is the smallest subarray
   minSubArrayLen([2,1,6,5,4], 9) // 2 -> because [5,4] is the smallest subarray
   minSubArrayLen([3,1,7,11,2,9,8,21,62,33,19], 52) // 1 -> because [62] is greater than 52
