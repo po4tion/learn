0efc701fb88ba9c4513b5f1c9252600a139c37df
po4tion <po4tion0429@gmail.com>
2024-05-20 23:36:20 +0900

    feat(recursion): someRecursive

---
+++ data-structure/recursion/someRecursive.js
@@ -0,0 +1,25 @@
+function someRecursive(list, callback) {
+  if (list.length === 0) {
+    return false;
+  }
+
+  if (callback(list[0])) {
+    return true;
+  }
+
+  return someRecursive(list.slice(1), callback);
+}
+
+// SAMPLE INPUT / OUTPUT
+// const isOdd = val => val % 2 !== 0;
+
+// someRecursive([1,2,3,4], isOdd) // true
+// someRecursive([4,6,8,9], isOdd) // true
+// someRecursive([4,6,8], isOdd) // false
+// someRecursive([4,6,8], val => val > 10); // false
+
+const isOdd = (val) => val % 2 !== 0;
+console.log(someRecursive([1, 2, 3, 4], isOdd));
+console.log(someRecursive([4, 6, 8, 9], isOdd));
+console.log(someRecursive([4, 6, 8], isOdd));
+console.log(someRecursive([4, 6, 8], (val) => val > 10));
